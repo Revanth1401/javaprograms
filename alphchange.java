965e6267fd1fa5e794101741e451dbb76cc4c92a
Revanth1401 <karamalarevanth9492@gmail.com>
2024-10-02 12:04:13 +0530

    Update alphchange.java

--- alphchange.java
+++ alphchange.java
@@ -1,23 +1,24 @@
 package Revanth;
-
-public class alphchange {
+import java.util.*;
+public class alphachange {
 	public static void main(String[] args) {
-		String s="abc-XYZ@#$-abcdefg";
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();    //s="ABCD-XYZ-abcd-xyz972&-865";
 		String t="";
-		int k=1;
+		int k=sc.nextInt();       //k=1;
 		for(int i=0;i<s.length();i++) {
 			char c=s.charAt(i);
 			if(c>='A' && c<='Z') {
 				int r=c-65;
 				r=(r+k)%26;
 				r=r+65;
-				t=t+((char)(r));
+				t=t+(char)(r);
 			}
 			else if(c>='a' && c<='z') {
-				int l=c-97;
-				l=(l+k)%26;
-				l=l+97;
-				t=t+((char)(l));
+				int r=c-97;
+				r=(r+k)%26;
+				r=r+97;
+				t=t+(char)(r);
 			}
 			else {
 				t=t+c;
